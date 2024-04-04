36ff870ae1bab0375671bd9bc6837c0d5e956fae
SuycxZMZ <suycx65@163.com>
2024-04-04 10:59:20 +0800

    provider pase message

--- src/rpcprovider.cc
+++ src/rpcprovider.cc
@@ -1,5 +1,6 @@
 #include "rpcprovider.h"
 #include "mprpcapplication.h"
+#include "rpcheader.pb.h"
 
 void RpcProvider::NotifyService(google::protobuf::Service * service)
 {
@@ -44,12 +45,59 @@ void RpcProvider::Run()
     m_eventLoop.loop();
 }
 
-void RpcProvider::OnConnection(const muduo::net::TcpConnectionPtr&)
+void RpcProvider::OnConnection(const muduo::net::TcpConnectionPtr &conn)
 {
-
+    if (!conn->connected())
+    {
+        conn->shutdown();
+    }
 }
 
-void RpcProvider::OnMessage(const muduo::net::TcpConnectionPtr&, muduo::net::Buffer*, muduo::Timestamp)
+// 处理远程调用请求
+/*
+service_name method_name args 
+单个请求帧格式 header_size + header(service_name method_name) + args_size + args
+args_size 可以防止tcp粘包。
+*/
+void RpcProvider::OnMessage(const muduo::net::TcpConnectionPtr &conn, 
+                            muduo::net::Buffer * buffer, 
+                            muduo::Timestamp time)
 {
+    std::string recv_buf = buffer->retrieveAllAsString();
 
+    // 读取头部大小
+    uint32_t header_size = 0;
+    recv_buf.copy((char*)&header_size, 4, 0);
+
+    // 读取头部原始字符流
+    std::string header_str = recv_buf.substr(4, header_size);
+
+    // 反序列化
+    mprpc::RpcHeader rpcHeader;
+    std::string service_name;
+    std::string method_name;
+    uint32_t args_size;
+    if (rpcHeader.ParseFromString(header_str))
+    {
+        service_name = rpcHeader.service_name();
+        method_name = rpcHeader.method_name();
+        args_size = rpcHeader.args_size();
+    }
+    else
+    {
+        // 反序列化失败
+        std::cout << "rpcHeader header_str : " << header_str << " pase error !!!" << std::endl;
+        return; 
+    }
+    // 取出参数
+    std::string args_str = recv_buf.substr(4 + header_size, args_size);
+
+    // 调试信息
+    std::cout << "=======================================" << std::endl;
+    std::cout << "header_size : " << header_size << std::endl;
+    std::cout << "service_name : " << service_name << std::endl;
+    std::cout << "method_name : " << method_name << std::endl;
+    std::cout << "args_size : " << args_size << std::endl;
+    std::cout << "args_str : " << args_str << std::endl;
+    std::cout << "=======================================" << std::endl;
 }
