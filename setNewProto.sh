f0baa8073c283df237d91f0d203458e108535eb2
SuycxZMZ <suycx65@163.com>
2024-06-08 15:58:07 +0800

    整理了一下目录结构

--- setNewProto.sh
+++ setNewProto.sh
@@ -4,14 +4,9 @@ set -x
 # 切换到 src 文件夹
 cd src
 
-rm -rf *.pb.cc *.pb.h
-rm -rf /include/*.pb.h
+rm -rf *.pb.h
+rm -rf *.pb.h
 
 protoc --cpp_out=./ *.proto
-mv *.pb.h /include/
 
-# 切换到 example 文件夹
-cd example
-rm -rf *.pb.cc *.pb.h
-protoc --cpp_out=./ *.proto
 
